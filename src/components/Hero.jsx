084af1f7c9b5a723e1ddaad810aa50ee0495936b
Joseph Billings <josephbillings.gb@gmail.com>
2024-07-11 13:12:54 -0600

    created navbar including left and right buttons with optional collapse at smaller sizes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,19 @@
+import { Container, Row, Col } from "react-bootstrap";
+
 export default function Hero({ title, lead }) {
   return (
-    <section>
-      <h1>{title}</h1>
-      <p>{lead}</p>
-    </section>
+    <Container fluid className="text-center">
+      <Row>
+        <Col xs="12" md={{ span: 8, offset: 2 }} lg={{ span: 6, offset: 3 }}>
+          <h1>{title}</h1>
+          <p>{lead}</p>
+        </Col>
+      </Row>
+    </Container>
+
+    // <section>
+    //   <h1>{title}</h1>
+    //   <p>{lead}</p>
+    // </section>
   );
 }
--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,21 +1,28 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+
 export default function MainNav({ brand, leftLinks, rightLinks }) {
   return (
-    <nav>
-      <a href="/">{brand}</a>
-      <ul>
-        {leftLinks.map(({ url, text }) => (
-          <li key={text}>
-            <a href={url}>{text}</a>
-          </li>
-        ))}
-      </ul>
-      <ul>
-        {rightLinks.map(({ url, text }) => (
-          <li key={text}>
-            <a href={url}>{text}</a>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <Navbar expand="lg" className="bg-success navbar-dark">
+      <Container>
+        <Navbar.Brand href="/">{brand}</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav className="me-auto">
+            {leftLinks.map(({ url, text }, index) => (
+              <Nav.Link key={index} href={url}>
+                {text}
+              </Nav.Link>
+            ))}
+          </Nav>
+          <Nav>
+            {rightLinks.map(({ url, text }, index) => (
+              <Nav.Link key={index} href={url}>
+                {text}
+              </Nav.Link>
+            ))}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
